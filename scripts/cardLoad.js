cf99ae86bd41ab155524392e981009ab7bebe348
Daniel-P-Arevalo <dparevalo@ucsd.edu>
2025-03-16 11:18:40 -0700

    Refactor card content load for easier modification
    Refactored the loadCard function to make it easier to modify in case of additional customizable fields for project cards

--- scripts/cardLoad.js
+++ scripts/cardLoad.js
@@ -5,44 +5,18 @@ function loadContent(card) {
     let description = card.getAttribute('description');
     let img = card.getAttribute('img');
     let imgAlt = card.getAttribute('imgAlt');
+    
+    card.querySelector('h2').innerHTML = title;
+    card.querySelector('time').innerHTML = date;
+    card.querySelector('a').setAttribute('href', link);
+    card.querySelector('p').innerHTML = description;
 
-    const style = document.createElement('style');
-    style.textContent = `
-            project-card {
-                display: flex;
-                flex-direction: row;
-                justify-content: space-between;
-                background-color: white;
-                border: solid black 0.25em;
-                border-radius: 1em;
-                margin: 1em;
-                padding: 1em;
-            }
-            project-card h2 {
-                margin-left: 0em;
-            }
-            project-card p {
-                width: 80%;
-            }`;
-    card.innerHTML = '';
-    card.appendChild(style);
-    card.innerHTML += `
-            <div class="project-text">
-                <hgroup>
-                    <h2>${title}</h2>
-                    <time>${date}</time>
-                </hgroup>
-                <br>
-                <a href="${link}">Further Reading</a>
-                <br><br>
-                <p>${description}</p>
-            </div>
-            <picture>
-                <source srcset="${img}" media="(max-width: 425px)" width="275" height="200">
-                <source srcset="${img}" media="(max-width: 768px)" width="650" height="450">
-                <source srcset="${img}" media="(max-width: 1024px)" width="750" height="550">
-                <img src="${img}" alt="${imgAlt}" width="600" height="400">
-            </picture>`;
+    let sources = card.querySelectorAll('source');
+    sources[0].setAttribute('srcset', img);
+    sources[1].setAttribute('srcset', img);
+    sources[2].setAttribute('srcset', img);
+    card.querySelector('img').setAttribute('src', img);
+    card.querySelector('img').setAttribute('alt', imgAlt);
 }
 
 function loadLocal() {
